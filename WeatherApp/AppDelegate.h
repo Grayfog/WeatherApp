8754340169a26f311cef879de1de86a90de65f68
Dmitry <u.dmitry52@gmail.com>
2017-06-22 19:58:46 +0300

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Дмитрий Юргель on 22.06.17.
+//  Copyright © 2017 Дмитрий Юргель. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Дмитрий Юргель on 22.06.17.
+//  Copyright © 2017 Дмитрий Юргель. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
